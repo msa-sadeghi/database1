4f9c2d645f20d47acc592e9822409e9044d5fdba
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-09-22 18:57:11 +0330

    load students added

--- student_form_ui.py
+++ student_form_ui.py
@@ -1,8 +1,6 @@
 from PyQt5.QtWidgets import QDialog, QMessageBox, QTableWidgetItem
 from student_form import Ui_Dialog
-
 from db import Database
-
 class StudentForm(QDialog):
     def __init__(self):
         super().__init__()
@@ -12,16 +10,13 @@ class StudentForm(QDialog):
         self.load_students()
 
     def load_students(self):
-        
         rows = self.db.fetchall('SELECT * FROM students')
-        self.ui.table_students.setRowCount(len(rows))
         self.ui.table_students.setColumnCount(len(dict(rows[0]).keys()))
         self.ui.table_students.setHorizontalHeaderLabels(dict(rows[0]).keys())
         for row_num, row in enumerate(rows):
             self.ui.table_students.insertRow(row_num)
             col_index = 0
             for col_num, data in dict(row).items():
-               
                 self.ui.table_students.setItem(row_num, col_index, QTableWidgetItem(str(data)))
                 col_index += 1
 
